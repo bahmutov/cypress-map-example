2cb14ce4b02ef25a1fa1f0f79ea575ef82d62efc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-12-08 06:53:51 -0500

    cypress-plugins

--- cypress/e2e/prices-list.cy.js
+++ cypress/e2e/prices-list.cy.js
@@ -1,18 +1,19 @@
 /// <reference types="cypress" />
 
-import 'cypress-map'
+// import cypress-map plugin
 
 it('adds all prices together', () => {
   cy.visit('cypress/prices-list.html')
-  cy.get('#total')
-    .should('have.attr', 'data-total')
-    .then(parseInt)
-    .then((total) => {
-      cy.get('#prices li')
-        .mapInvoke('getAttribute', 'data-price')
-        .map(parseInt)
-        .tap()
-        .reduce((sum, n) => sum + n)
-        .should('equal', total)
-    })
+  // get the element with the total price
+  // and extract the total in cents
+  // from its "data-total" attribute
+  // parse the String into a number
+  // then use the "total" in a cy.then callback
+  //
+  // get all price list items
+  // call the "getAttribute" "data-price" method
+  // on each element, then parse each string
+  // into a number and reduce to the sum
+  //
+  // confirm the sum is equal to the total
 })
