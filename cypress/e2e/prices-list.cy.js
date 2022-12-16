7ba46b4ef0ebcbe38c1aa4c9ddb8c3564ec7e664
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-12-16 11:00:44 -0500

    solved

--- cypress/e2e/prices-list.cy.js
+++ cypress/e2e/prices-list.cy.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 // import cypress-map plugin
+import 'cypress-map'
 
 it('has the last item', () => {
   cy.visit('cypress/prices-list.html')
@@ -64,4 +65,31 @@ it('confirms the sum is correct', () => {
   // call the "getAttribute" "data-price" method
   // on each element, then parse each string
   // into a number and reduce to the sum
+})
+
+it('gets the raw DOM element at position k', () => {
+  cy.visit('cypress/prices-list.html')
+  // get the prices LI elements
+  // and confirm it is a jQuery object
+  // https://on.cypress.io/get
+  // "should satisfy" assertion
+  // https://glebbahmutov.com/cypress-examples/commands/assertions.html
+  // https://on.cypress.io/dom
+  cy.get('#prices li')
+    .should('satisfy', Cypress.dom.isJquery)
+    // yield the first element
+    // https://on.cypress.io/eq
+    // https://on.cypress.io/first
+    // and confirm it is _still_ a jQuery object
+    .first()
+    .should('satisfy', Cypress.dom.isJquery)
+  // Tip: can you debug the Cypress.dom static methods?
+  //
+  // Now change the standard Cypress queries to cypress-map
+  // and confirm they yield the DOM elements
+  cy.get('#prices li')
+    .should('satisfy', Cypress.dom.isJquery)
+    .at(0) // or ".primo()"
+    .should('satisfy', Cypress.dom.isElement)
+    .and('have.property', 'innerText', 'Oranges $0.99')
 })
