f5432e54baede2351ed90ba85d7d24f1b208458a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-12-19 19:20:57 -0500

    add comments

--- cypress/e2e/json-attribute.cy.js
+++ cypress/e2e/json-attribute.cy.js
@@ -2,4 +2,8 @@
 
 it('confirms the attribute age', () => {
   cy.visit('cypress/json-attribute.html')
+  // find the element with id "person"
+  // it should have an attribute "data-field"
+  // inside should be a stringified JSON object
+  // with property 'age: 10'
 })
