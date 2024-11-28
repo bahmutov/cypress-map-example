d183b8a1c35c2160c641de420dd14d8eb5b7fe1b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-11-28 09:41:00 -0500

    starting test

--- cypress/e2e/prices-list.cy.js
+++ cypress/e2e/prices-list.cy.js
@@ -1,18 +1,12 @@
 /// <reference types="cypress" />
 
 // import cypress-map plugin
-import 'cypress-map'
 
 it('checks the exact match items', () => {
   cy.visit('cypress/prices-list.html')
   // confirm the LI elements have text
   // ['Oranges $0.99', 'Mango $1.01', 'Potatoes $0.20']
   // Tip: use the "should read" assertion from the cypress-map plugin
-  cy.get('li').should('read', [
-    'Oranges $0.99',
-    'Mango $1.01',
-    'Potatoes $0.20',
-  ])
 })
 
 it('check each item against a regular expression', () => {
@@ -20,6 +14,4 @@ it('check each item against a regular expression', () => {
   // confirm each LI item has text with a word plus a price like "$0.99"
   // Tip: use a regular expression to match the text
   // and the "should read" assertion from the cypress-map plugin
-  const nameWithPrice = /^\w+ \$\d+\.\d{2}$/
-  cy.get('li').should('read', [nameWithPrice, nameWithPrice, nameWithPrice])
 })
